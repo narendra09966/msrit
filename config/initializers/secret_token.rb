6f0756d302878dc1c9704e28a653c726909a70fb
hduser <hduser@slave1.(none)>
2013-05-04 11:08:21 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Msrit::Application.config.secret_token = '9f2c6984043064b58044de4f6ca774baf3fdb087bf2b51abc0e46a20f9a49228554f90ff948b50abf7c0cc02a24bd61ed20650c1c52209319ad654a6e9ff161c'
